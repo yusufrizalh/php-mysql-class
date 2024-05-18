1e3c1ef72acd46a06325f3be55a902a3ea702976
Yusuf Rizal <yusufrizalh@gmail.com>
2024-05-19 00:17:06 +0700

    change fav icon

--- layouts/header.php
+++ layouts/header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="https://avatars.githubusercontent.com/u/18276695?v=4">
     <title>My Simple Web</title>
     <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
